111dac2c6a0f94bf31fac015be9266b401ee286a
Matthew Woolley <matthew@fortune-studios.com>
2015-12-11 20:54:40 +0000

    Implemented Character Into Game

--- Source/Road_Fever/Private/GameModes/RoadFeverGameMode.cpp
+++ Source/Road_Fever/Private/GameModes/RoadFeverGameMode.cpp
@@ -2,7 +2,12 @@
 
 #include "Road_Fever.h"
 #include "RoadFeverGameMode.h"
+#include "Public/Characters/RoadFeverCharacterNed.h"
 
 
 
-
+// Called when this GameMode enters memory. [11/12/2015 Matthew Woolley]
+ARoadFeverGameMode::ARoadFeverGameMode()
+{
+	DefaultPawnClass = ARoadFeverCharacterNed::StaticClass();
+}
--- Source/Road_Fever/Public/GameModes/RoadFeverGameMode.h
+++ Source/Road_Fever/Public/GameModes/RoadFeverGameMode.h
@@ -13,7 +13,8 @@ class ROAD_FEVER_API ARoadFeverGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
-	
-	
+public:
+	// Called when this GameMode enters memory. [11/12/2015 Matthew Woolley]
+	ARoadFeverGameMode();
 	
 };
