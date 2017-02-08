62abf4bda089e2278cfc54cd8c3dbdc46fa955de
Matthew Woolley <matthewwoolley31@gmail.com>
2017-02-08 19:38:39 +0000

    Removed destroying from the death method

--- Source/Road_Fever/Private/AI/Enemy/RoadFeverEnemy.cpp
+++ Source/Road_Fever/Private/AI/Enemy/RoadFeverEnemy.cpp
@@ -8,10 +8,12 @@
 // Setup default values for this enemy. [15/7/2016 Matthew Woolley]
 ARoadFeverEnemy::ARoadFeverEnemy()
 {
+	// Set-up the enemy's stats. [8/2/2017 Matthew Woolley]
 	EnemyHealth = 100;
 	MinMoveSpeed = 1;
 	MaxMoveSpeed = 1;
 
+	// Set-up the mesh so it isn't orientated strangely. [8/2/2017 Matthew Woolley]
 	GetMesh()->SetRelativeLocation( FVector( 0, 0, -90 ) );
 	GetMesh()->SetRelativeRotation( FRotator( 0, -90, 0 ) );
 }
@@ -36,19 +38,16 @@ void ARoadFeverEnemy::BeginPlay()
 // Called when this enemy no longer has any health. [15/7/2016 Matthew Woolley]
 void ARoadFeverEnemy::Die_Implementation()
 {
-	// If this enemy isn't already being killed. [15/7/2016 Matthew Woolley]
-	if ( !IsPendingKill() && EnemyHealth <= 0 )
-	{
-		// Kill it. [15/7/2016 Matthew Woolley]
-		Destroy();
-	}
+	// No default implementation of this method. [8/2/2017 Matthew Woolley]
 }
 
 // Takes away health from this enemy. [15/7/2016 Matthew Woolley]
 void ARoadFeverEnemy::TakeDamage_Implementation( int32 InDamage )
 {
+	// Deal damage to the enemy. [8/2/2017 Matthew Woolley]
 	EnemyHealth -= InDamage;
 
+	// Check if the enemy has died. [8/2/2017 Matthew Woolley]
 	if ( EnemyHealth <= 0 )
 	{
 		Die();
